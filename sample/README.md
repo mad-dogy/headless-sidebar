5806cd507881699b74f9648e06946f7ab3d9d3b2
Лолита <120950389+mad-dogy@users.noreply.github.com>
2024-10-25 13:44:38 +0300

    setup base configuration

---
+++ sample/README.md
@@ -0,0 +1,1 @@
+# Headless sidebar sample
---
+++ sample/src/App.tsx
@@ -0,0 +1,11 @@
+import { Sidebar } from 'headless-sidebar';
+
+import './App.css';
+
+function App() {
+  return (
+    <Sidebar/>
+  )
+}
+
+export default App;
